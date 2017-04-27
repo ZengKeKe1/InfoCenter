68c150d0511b876607a55880e21732550d03c139
driver1998 <driver1998@foxmail.com>
2017-04-27 22:16:17 +0800

    initial release, build 20170423 first commit to git

---
+++ ScoreQuery/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace ScoreQuery
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string server;
+        public string Server
+        {
+            get => server;
+            set
+            {
+                var regex = new System.Text.RegularExpressions.Regex(@"^(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)$");        
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("invalid ip");
+                }
+                else
+                {
+                    server = value;
+                }
+                
+            }
+        }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var connString = "Database='高数成绩';Data Source=SERVER;User ID=root;Password=123456;CharSet=utf8";
+            connString = connString.Replace("SERVER", Server);
+            var sqlConnection = new MySqlConnection(connString);
+            try
+            {
+                await sqlConnection.OpenAsync();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("服务器连接失败。");
+                sqlConnection.Close();
+                return;
+            }
+            
+
+            var sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "SELECT * FROM admin where 学号=@user and 密码=@pass";
+            sqlCommand.Parameters.Add(new MySqlParameter("@user", Username));
+            sqlCommand.Parameters.Add(new MySqlParameter("@pass", pwd.Password));
+
+            var reader = sqlCommand.ExecuteReader();
+            if (!reader.HasRows)
+            {
+                MessageBox.Show("密码错误!");
+                sqlConnection.Close();
+            }
+            else
+            {
+                sqlConnection.Close();
+                
+                var window = new Window1();
+                window.Server = Server;
+                window.Username = Username.Substring(Username.Length - 4, 4);
+                
+                window.Show();
+                this.Close();
+            }
+            
+        }
+    }
+}
---
+++ ScoreQuery/Window1.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace ScoreQuery
+{
+    public class Subject
+    {
+        public string SubjectName { get; set; }
+        public string Server { get; set; }
+        
+        public async Task<List<Score>> GetScore(String username)
+        {
+            var connString = "Database='高数成绩';Data Source=SERVER;User ID=usr;Password=user123456;CharSet=utf8";
+            connString = connString.Replace("SERVER", Server);
+            
+            var sqlConnection = new MySqlConnection(connString);
+
+            
+            try
+            {
+                await sqlConnection.OpenAsync();
+            }
+            catch (Exception)
+            {
+                sqlConnection.Close();
+                throw;
+            }
+            
+            var sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "SELECT * FROM 高数成绩 WHERE 学号=@user";
+            sqlCommand.Parameters.Add(new MySqlParameter("@user", username));
+            //sqlCommand.Parameters.Add(new MySqlParameter("@table", SubjectName));
+
+            DbDataReader reader = null;
+
+            try
+            {
+                reader = await sqlCommand.ExecuteReaderAsync();
+                var a = reader.HasRows;
+                await reader.ReadAsync();
+            }
+            catch (Exception)
+            {
+                sqlConnection.Close();
+                throw;
+            }
+
+            if (reader == null) return null;
+
+            var count = reader.FieldCount;
+            var scores = new List<Score>();
+            int i;
+            for (i = 3; i < count; i++)
+            {
+                if (reader.IsDBNull(i)) continue;
+                var score = new Score()
+                {
+                    Name = reader.GetString(1),
+                    Description = reader.GetName(i),
+                    Points = reader.GetInt32(i)
+                };
+
+                scores.Add(score);
+            }
+            sqlConnection.Close();
+            return scores;
+        }
+    }
+    public class Score
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Points { get; set; }
+    }
+    
+    
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string Server { get; set; }
+        public string Username { get; set; }
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var subject = new Subject()
+            {
+                Server = this.Server,
+                SubjectName = "高数成绩"
+            };
+
+            List<Score> scores = null;
+
+            try
+            {
+                scores = await subject.GetScore(Username); 
+            }
+            catch (DbException)
+            {
+                MessageBox.Show("服务器连接失败！");
+                return;
+            }
+
+            if (scores == null || scores.Count ==0)
+            {
+                MessageBox.Show("您好像没有成绩...");
+            }
+            else
+            {
+                List.ItemsSource = scores;
+                this.DataContext = scores[1];
+            }
+            
+        }
+    }
+}
